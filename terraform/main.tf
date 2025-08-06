d00c5ac27583a41059a8af2222d6bc90ba9cf764
as3ii <as3ii777@gmail.com>
2025-08-06 11:12:08 +0200

    Updated proxmox terraform provider from 2.9 to 3.0.2-rc03, refactored main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,35 @@ variable "proxmox_node" {
   description = "Proxmox cluster node name"
 }
 
+variable "nodes" {
+  type = set(object({
+    name     = string                               # Unique name of the VM
+    id       = optional(number, 0)                  # Unique id of the VM, 0 = the first available one
+    template = string                               # Name of the VM template to clone
+    network  = optional(string, "ip=dhcp,ip6=auto") # Network settings, eg: 'ip=10.0.0.100,gw=10.0.0.1'
+  }))
+  description = "VMs settings, see default"
+  default = [
+    {
+      name     = "swarm1"
+      id       = 0
+      template = "debian12"
+      network  = "ip=dhcp,ip6=auto"
+    }
+  ]
+  validation {
+    condition = alltrue([
+      for n in var.nodes : alltrue([
+        length(n.name) > 0,
+        n.id >= 0,
+        length(n.template) > 0,
+        can(regex("^((ip=([0-9]{1,3}[.]){3}[0-9]{1,3}/[0-9]+,gw=([0-9]{1,3}[.]){3}[0-9]{1,3})|(ip=dhcp))?,?((ip6=.*/[0-9]+,gw6=.*)|(ip6=dhcp)|(ip6=auto))?$", n.network)),
+      ])
+    ])
+    error_message = "Invalid nodes attributes"
+  }
+}
+
 variable "ssh_public_keys" {
   type        = list(string)
   description = "SSH public key files"
@@ -18,57 +47,89 @@ variable "password" {
   }
 }
 
-variable "template" {
-  type        = string
-  description = "Template path and name"
-  default     = "local:vztmpl/debian-12-standard_12.7-1_amd64.tar.zst"
-  validation {
-    condition     = can(regex("[[:alnum:]]+:[[:alnum:]]+/[[:graph:]]+[.]tar[.](zstd?|xz|gz)", var.template))
-    error_message = "Invalid template path"
+resource "proxmox_vm_qemu" "swarm" {
+  for_each = {
+    for i, n in var.nodes : n.name => n
   }
-}
 
-resource "proxmox_vm_qemu" "swarm1" {
   target_node = var.proxmox_node
-  name        = "swarm1"
-  #description  = ""
-  tags       = "swarm"
-  vmid       = 0 # next available one
-  ostemplate = var.template
-  onboot     = true
-  vm_state   = "running"
+  name        = each.value.name
+  tags        = "swarm"
+  vmid        = each.value.id
+  onboot      = true
+  vm_state    = "running"
+  agent       = 1 # Enable QEMU guest agent
 
-  iso = ""
+  # VM template to clone
+  clone      = each.value.template
+  full_clone = true
 
-  sockets = 1
-  cores   = 2
-  #cpulimit = 4
-  #cpuunits = 1024
-  memory = 2048
-  swap   = 512
+  # CPU and RAM
+  cpu {
+    sockets = 1
+    cores   = 2
+    type    = "x86-64-v2-AES" # Use "host" to copy the host CPU type
+    #limit   = 4
+    #units   = 1024
+  }
+  memory  = 2048
+  balloon = 256 # Minimum allocated memory
 
+  # High Availability settings
   #hastate = ""
   #hagroup = ""
 
+  # Cloud-init settings
   os_type    = "cloud-init"
   ssh_user   = "root"
   ciuser     = "root"
   cipassword = var.password
   sshkeys    = join("\n", [for p in var.ssh_public_keys : file(p)])
+  ipconfig0  = each.value.network
+  skip_ipv6  = true # Acquiring an IPv6 address from the qemu guest agent isn't required
+  #nameserver = ""
+  #searchdomain = ""
 
-  disk {
-    type      = "disk"
-    disk_file = "local-lvm:vm-<<<vmid>>>-disk-<<<disk number>>>"
-    slot      = "scsi0"
+  # HW settings
+  bios = "seabios"
+  serial {
+    id   = 0
+    type = "socket"
+  }
+  vga {
+    type = "serial0"
+  }
+  boot   = "order=scsi0"
+  scsihw = "virtio-scsi-single"
+
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          storage    = "local"
+          size       = "3G"
+          iothread   = true
+          discard    = true
+          emulatessd = true
+          format     = "qcow2"
+        }
+      }
+    }
+    ide {
+      ide0 {
+        cloudinit {
+          storage = "local"
+        }
+      }
+    }
   }
 
   network {
     id     = 0
     model  = "virtio"
     bridge = "vmbr0"
+    #tag    = 100 # VLAN
   }
-  #searchdomain = ""
-  #nameserver = ""
 }
 
 # vim: sw=2
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,7 +4,8 @@ terraform {
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
-      version = "~> 2.9.14"
+      version = "3.0.2-rc03"
+      #version = "~> 3.0.2"
     }
   }
 }
@@ -37,8 +38,18 @@ provider "proxmox" {
   pm_api_url          = var.proxmox_api_url
   pm_api_token_id     = var.proxmox_api_token_id
   pm_api_token_secret = var.proxmox_api_token_secret
+  #pm_parallel         = 2
 
   pm_tls_insecure = true
+
+  # Debug
+  # pm_log_enable = true
+  # pm_log_file   = "terraform-plugin-proxmox.log"
+  # pm_debug      = true
+  # pm_log_levels = {
+  #   _default    = "debug"
+  #   _capturelog = ""
+  # }
 }
 
 # vim: sw=2
